4e1ad908ba7f17413f51d313f0d1f5fb92aad378
Max Zhang <48145854+chetbae@users.noreply.github.com>
2023-06-09 22:08:19 -0400

    add colors.ts, change typography to .ts

---
+++ src/color/color.ts
@@ -0,0 +1,10 @@
+const color = {
+    primary: "#000000",
+    secondary: "#629FFC",
+    secondaryLight: "#D7E9FF",
+    tertiary: "#FFF6E3",
+    quaternary: "#D6D6D6",
+    background: "#FFFFFF",
+};
+
+export default color;
