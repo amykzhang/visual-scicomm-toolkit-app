bf0528c24edb9f14eeabdc32bbb38bdf0645a822
Max Zhang <48145854+chetbae@users.noreply.github.com>
2023-08-19 00:10:12 -0400

    remove useImage Hook

--- src/components/ImageTool.tsx
+++ src/components/ImageTool.tsx
@@ -3,7 +3,6 @@ import Konva from "konva";
 import styled from "styled-components";
 import { ImageProp } from "../utils/interfaces";
 import { v4 as uuid } from "uuid";
-import useImage from "use-image";
 
 const StyledImage = styled.img`
     cursor: move; /* fallback if grab cursor is unsupported */
@@ -35,27 +34,31 @@ export const ImageTool: FC<ImageToolProps> = ({
     setImages,
     stageRef,
 }) => {
-    const [image] = useImage(src);
+    function putImageOnCanvas(
+        x: number,
+        y: number,
+        offset: { x: number; y: number }
+    ) {
+        const imageElement = new Image();
+        imageElement.width = dimensions.width;
+        imageElement.height = dimensions.height;
+        imageElement.src = src;
 
-    function putImageOnCanvas(x: number, y: number) {
-        if (image !== undefined) {
-            image.width = dimensions.width;
-            image.height = dimensions.height;
-
-            setImages([
-                ...images,
-                {
-                    id: uuid(),
-                    x: x,
-                    y: y,
-                    width: dimensions.width,
-                    height: dimensions.height,
-                    rotation: 0,
-                    isDragging: false,
-                    image: image,
-                },
-            ]);
-        }
+        setImages([
+            ...images,
+            {
+                id: uuid(),
+                x: x,
+                y: y,
+                width: dimensions.width,
+                height: dimensions.height,
+                rotation: 0,
+                isDragging: false,
+                image: imageElement,
+                src: src,
+                offset: offset,
+            },
+        ]);
     }
     const handleImageClick = () => {
         console.log("handleImageClick", dimensions.width, dimensions.height);
@@ -70,12 +73,12 @@ export const ImageTool: FC<ImageToolProps> = ({
         console.log("handleImageDragEnd");
         if (stageRef.current !== null) {
             const stage = stageRef.current;
-            const x =
-                (e.clientX - stage.x()) / stage.scaleX() - dimensions.width / 2;
-            const y =
-                (e.clientY - stage.y()) / stage.scaleX() -
-                dimensions.height / 2;
-            putImageOnCanvas(x, y);
+            const x = (e.clientX - stage.x()) / stage.scaleX();
+            const y = (e.clientY - stage.y()) / stage.scaleX();
+            putImageOnCanvas(x, y, {
+                x: dimensions.width / 2,
+                y: dimensions.height / 2,
+            });
         }
     };
 
