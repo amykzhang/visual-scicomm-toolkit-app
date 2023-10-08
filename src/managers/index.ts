89818c8fdf92d465291a2f126c353af3c41e8f9f
chetbae <48145854+chetbae@users.noreply.github.com>
2023-10-08 17:37:25 -0400

    Remove keypressmanager export

--- src/managers/index.ts
+++ src/managers/index.ts
@@ -1,7 +1,7 @@
 export { StageViewManager } from "./StageViewManager";
 export { CommentViewManager } from "./CommentViewManager";
-export { KeyPressManager } from "./KeyPressManager";
 export { ExportManager } from "./ExportManager";
 export { SelectionManager } from "./SelectionManager";
 export { TextViewManager } from "./TextViewManager";
 export { DragSelectManager } from "./DragSelectManager";
+export { DrawViewManager } from "./DrawViewManager";
