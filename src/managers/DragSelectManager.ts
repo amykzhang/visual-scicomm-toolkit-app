f3a089f607b14c053633788a1b41c399d2c55ee5
chetbae <48145854+chetbae@users.noreply.github.com>
2023-10-11 01:28:34 -0400

    Change selection bound algorithm for any intersection

--- src/managers/DragSelectManager.ts
+++ src/managers/DragSelectManager.ts
@@ -72,15 +72,37 @@ export const DragSelectManager = (
             height: Math.abs(selectionBounds.height),
         };
 
-        return elements
-            .filter(
-                (element) =>
-                    element.x > actualBounds.x &&
-                    element.y > actualBounds.y &&
-                    element.x + element.width < actualBounds.x + actualBounds.width &&
-                    element.y + element.height < actualBounds.y + actualBounds.height
-            )
+        const filteredElementIds = elements
+            .filter((element) => {
+                // return (
+                //     element.x > actualBounds.x &&
+                //     element.y > actualBounds.y &&
+                //     element.x + element.width < actualBounds.x + actualBounds.width &&
+                //     element.y + element.height < actualBounds.y + actualBounds.height
+                // );
+
+                const A = {
+                    x0: element.x,
+                    y0: element.y,
+                    x1: element.x + element.width,
+                    y1: element.y + element.height,
+                };
+                const B = {
+                    x0: actualBounds.x,
+                    y0: actualBounds.y,
+                    x1: actualBounds.x + actualBounds.width,
+                    y1: actualBounds.y + actualBounds.height,
+                };
+                const aLeftOfB = A.x1 < B.x0;
+                const aRightOfB = A.x0 > B.x1;
+                const aAboveB = A.y0 > B.y1;
+                const aBelowB = A.y1 < B.y0;
+
+                return !(aLeftOfB || aRightOfB || aAboveB || aBelowB);
+            })
             .map((element) => element.id);
+
+        return filteredElementIds;
     }
 
     return {
